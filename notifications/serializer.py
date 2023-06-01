bf1404ec4d8bfa6633813f616667eba357242b20
Mossaddak <mossaddak15-2413@diu.edu.bd>
2023-06-01 11:08:02 +0600

    notification serializer added

--- notifications/serializer.py
+++ notifications/serializer.py
@@ -4,10 +4,12 @@ from rest_framework.serializers import (
 from rest_framework import (
     serializers
 )
+from authentication.serializers.base_auth import UserCreationSerializer
 from .models import Notification
-
+from django.contrib.auth import get_user_model
 
 class notificationSerializer(ModelSerializer):
+    user = UserCreationSerializer()
     class Meta:
         model = Notification
         fields = "__all__"
