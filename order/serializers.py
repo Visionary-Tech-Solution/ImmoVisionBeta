2a2e8f9d42fdaf48e750eaeaae1f978296f1af86
Mossaddak <mossaddak15-2413@diu.edu.bd>
2023-06-11 22:32:53 +0600

    updated

--- order/serializers.py
+++ order/serializers.py
@@ -4,10 +4,15 @@ from account.serializers.broker import BrokerProfileSerializer
 from account.serializers.freelancer import FreelancerProfileSerializer
 from order.models import Amount, BugReport, Commition, DiscountCode, Order
 
+from upload_video.serializer import(
+    VideoSerializer
+)
+
 
 class OrderSerializer(serializers.ModelSerializer):
     order_sender = BrokerProfileSerializer()
     order_receiver = FreelancerProfileSerializer()
+    #upload_video_url = VideoSerializer()
     class Meta:
         model = Order
         fields = '__all__'
