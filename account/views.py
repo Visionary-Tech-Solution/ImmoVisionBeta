0c91521cf7a4758d9311cdc9c28a767052d592f0
Asraful Islam Spondon <asrafulislamais@gmail.com>
2023-06-22 04:23:38 +0600

    Some Update

--- account/views.py
+++ account/views.py
@@ -1,18 +1,17 @@
 # from authentication.serializers import UserSerializerWithToken
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import make_password
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
-
 from account.models import BrokerProfile, FreelancerProfile, Profile
 from account.serializers.base import ProfileSerializer
 from account.serializers.broker import BrokerProfileSerializer
 from account.serializers.freelancer import FreelancerProfileSerializer
 from algorithm.auto_detect_freelancer import auto_detect_freelancer
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from order.views import Order
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
 User = get_user_model()
 def get_paginated_queryset_response(qs, request, user_type):
@@ -301,6 +300,7 @@ def admin_status_change(request, username):
     elif status_type == "suspended":
          for order in orders:
             order.order_receiver = None
+            order.status = "pending"
             order.save() 
     freelancer.status_type = status_type
     freelancer.save()
