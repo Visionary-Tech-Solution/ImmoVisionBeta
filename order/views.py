dc707ead106bf060d0b3eebccfab10249bb74c82
Asraful Islam Spondon <asrafulislamais@gmail.com>
2023-06-27 21:52:02 +0600

    Bug Update 8

--- order/views.py
+++ order/views.py
@@ -924,12 +924,12 @@ def make_payment(request):
     profile = Profile.objects.get(user=user)
 
     customer_id = profile.stripe_customer_id
+    customer = stripe.Customer.create()
     payment_type = profile.payment_type
     if customer_id is not None and len(customer_id) > 0:
         # charge_customer(customer_id, payment_type)
         pass
     else:
-        customer = stripe.Customer.create()
         customer_id = customer['id']
 
     try:
