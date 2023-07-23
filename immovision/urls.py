86ccd4966a42f3daac1552a2446f184b337ef13a
root <root@ip-10-0-13-122.us-east-2.compute.internal>
2023-07-23 04:18:20 +0000

    simple changes for static and media file handle

--- immovision/urls.py
+++ immovision/urls.py
@@ -51,4 +51,6 @@ urlpatterns = [
     path('api/notification/', include('notifications.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
