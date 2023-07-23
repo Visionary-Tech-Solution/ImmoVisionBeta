f75e9360a11ce802a3b1bb575c7428e97dc9449a
Asraful Islam Spondon <asrafulislamais@gmail.com>
2023-07-23 11:15:55 +0600

    Max Order Done

--- order/admin.py
+++ order/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from order.models import Amount, BugReport, Commition, DiscountCode, Order
+from order.models import (Amount, BugReport, Commition, DiscountCode, MaxOrder,
+                          Order)
 
 # Register your models here.
 admin.site.register(Order)
+admin.site.register(MaxOrder)
 admin.site.register(Commition)
 admin.site.register(BugReport)
 admin.site.register(DiscountCode)
