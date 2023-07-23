81cc72b4b3f61904cc38b55ccbd26820e734a053
Md Shahed Talukder <root@ip-10-0-13-122.us-east-2.compute.internal>
2023-07-23 05:02:42 +0000

    new commit

--- immovision/settings.py
+++ immovision/settings.py
@@ -145,7 +145,7 @@ CORS_ALLOW_ALL_ORIGINS=True
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
+if not  DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
