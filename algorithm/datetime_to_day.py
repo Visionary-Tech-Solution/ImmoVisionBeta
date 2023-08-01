85d9ccc1add6d873dc0402d3cfc82aadfdae6687
Asraful Islam Spondon <asrafulislamais@gmail.com>
2023-08-02 05:37:20 +0600

    Avg Percentage Solve

--- algorithm/datetime_to_day.py
+++ algorithm/datetime_to_day.py
@@ -12,14 +12,19 @@ def get_day_from_datetime(datetime):
     return day_name
 
 
-def get_day_name( day):
+def get_day_name(day_number):
     days_of_week = {
-    0: 'Monday',
-    1: 'Tuesday',
-    2: 'Wednesday',
-    3: 'Thursday',
-    4: 'Friday',
-    5: 'Saturday',
-    6: 'Sunday'
+        0: 'Monday',
+        1: 'Tuesday',
+        2: 'Wednesday',
+        3: 'Thursday',
+        4: 'Friday',
+        5: 'Saturday',
+        6: 'Sunday'
     }
-    return days_of_week[day]
+
+    mapped_day_number = day_number % 7  # Map day numbers greater than 6 to their corresponding day numbers
+    if mapped_day_number in days_of_week:
+        return days_of_week[mapped_day_number]
+    else:
+        return 'Invalid day number'
